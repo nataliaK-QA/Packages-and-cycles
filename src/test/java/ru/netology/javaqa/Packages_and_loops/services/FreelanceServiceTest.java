5429c788ea91902072825c2c3fc368c1e31647b5
Natalia Kashcheeva <n.kashcheeva96@gmail.com>
2025-02-02 14:37:37 +0300

    fix code commit

--- src/test/java/ru/netology/javaqa/Packages_and_loops/services/FreelanceServiceTest.java
+++ src/test/java/ru/netology/javaqa/Packages_and_loops/services/FreelanceServiceTest.java
@@ -9,19 +9,17 @@ public class FreelanceServiceTest {
 
 
     @ParameterizedTest
-    @CsvFileSource(files="src/test/resources/Vaftion.csv")
-    //@ParameterizedTest
-    //@CsvSource({
-     //       "10000, 3000, 20000, 3", // Пример из условия
-     //       "100000, 60000, 150000, 2" // Пример из условия
-    //})
+    @CsvFileSource(files = "src/test/resources/Vaftion.csv")
+        //@ParameterizedTest
+        //@CsvSource({
+        //       "10000, 3000, 20000, 3", // Пример из условия
+        //       "100000, 60000, 150000, 2" // Пример из условия
+        //})
     void testCalculateVacationMonths(int income, int expenses, int threshold, int expected) {
         FreelanceService service = new FreelanceService();
         int actual = service.calculateVacationMonths(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
-
-
 
 
 }
